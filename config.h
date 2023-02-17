0c62f7bda5362d0a2bd40a09a78bc1edd88c8a8c
ItsNotGoodName <gurnaindeol@gmail.com>
2023-02-16 19:44:24 -0800

    feat: systray patch

--- config.h
+++ config.h
@@ -9,6 +9,13 @@ static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 
+/* systray */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
+
 /* Gruvbox colors */
 static const char col_bg0[]       = "#3c3836";
 static const char col_bg1[]       = "#262626";
